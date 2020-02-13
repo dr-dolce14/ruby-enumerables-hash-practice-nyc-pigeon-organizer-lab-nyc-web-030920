f239229ca969b634fe4fbacf955c408034ad763f
Paola Dolcemascolo <dolcemascop1@gmail.com>
2020-02-13 16:05:55 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+data.each do |first_key, hash|
+  hash.each do |value, names|
+    names.each do |name|
+      if pigeon_list[name] == nil
+        pigeon_list[name] = {}
+      end
+      if pigeon_list[name][first_key] == nil
+        pigeon_list[name][first_key] = []
+      end
+      pigeon_list[name][first_key].push(value.to_s)
+    end
+  end
+end
+pigeon_list
 end
